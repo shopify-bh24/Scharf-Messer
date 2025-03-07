0c3b425590c413e95fdf709b988f32733a39c9a7
graywolf2419 <graywolf2419@gmail.com>
2025-03-07 07:48:37 -0800

    first commit

---
+++ assets/theme-editor.js
@@ -0,0 +1,73 @@
+function hideProductModal() {
+  const productModal = document.querySelectorAll('product-modal[open]');
+  productModal && productModal.forEach((modal) => modal.hide());
+}
+
+document.addEventListener('shopify:block:select', function (event) {
+  hideProductModal();
+  const blockSelectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockSelectedIsSlide) return;
+
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  parentSlideshowComponent.pause();
+
+  setTimeout(function () {
+    parentSlideshowComponent.slider.scrollTo({
+      left: event.target.offsetLeft,
+    });
+  }, 200);
+});
+
+document.addEventListener('shopify:block:deselect', function (event) {
+  const blockDeselectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockDeselectedIsSlide) return;
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
+});
+
+document.addEventListener('shopify:section:load', () => {
+  hideProductModal();
+  const zoomOnHoverScript = document.querySelector('[id^=EnableZoomOnHover]');
+  if (!zoomOnHoverScript) return;
+  if (zoomOnHoverScript) {
+    const newScriptTag = document.createElement('script');
+    newScriptTag.src = zoomOnHoverScript.src;
+    zoomOnHoverScript.parentNode.replaceChild(newScriptTag, zoomOnHoverScript);
+  }
+});
+
+document.addEventListener('shopify:section:reorder', () => hideProductModal());
+
+document.addEventListener('shopify:section:select', () => hideProductModal());
+
+document.addEventListener('shopify:section:deselect', () => hideProductModal());
+
+document.addEventListener('shopify:inspector:activate', () => hideProductModal());
+
+document.addEventListener('shopify:inspector:deactivate', () => hideProductModal());
+
+document.addEventListener('DOMContentLoaded', function () {
+  const variantSelector = document.querySelector('[name="id"]');
+  const mediaItems = document.querySelectorAll('.media-gallery__item');
+
+  if (variantSelector && mediaItems) {
+    variantSelector.addEventListener('change', function () {
+      const selectedVariant = this.value;
+     
+
+      mediaItems.forEach(item => {
+        const variantIds = item.getAttribute('data-variant-id').split(',');
+
+        if (variantIds.includes(selectedVariant)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    });
+
+    // Trigger change event on page load to show correct images
+    variantSelector.dispatchEvent(new Event('change'));
+  }
+});
+
